ce1ff5228a3ef766a66d2b0ea13cd0e5268a3ca4
Jangwoojun <woojun0107@naver.com>
2023-08-12 13:48:08 +0900

    first commit

---
+++ test.py
@@ -0,0 +1,10 @@
+
+import win32com.client
+
+
+objCpCybos = win32com.client.Dispatch("CpUtil.CpCybos")
+bConnect = objCpCybos.IsConnect
+if (bConnect == 0):
+    print("PLUS가 정상적으로 연결되지 않음. ")
+else:
+    print("PLUS가 정상적으로 연결됨.")
